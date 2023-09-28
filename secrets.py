c37d4891db449f7ee625751a79064f2cc874dd5e
RamandeepS <70764724+ramanveerji@users.noreply.github.com>
2023-09-28 19:51:43 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,18 +1,15 @@
 import os
-from dotenv import load_dotenv
 
-# Load environment variables from a .env file (if present)
-load_dotenv()
-
-# Retrieve values from environment variables
-BOT_TOKEN = os.getenv("TOKEN")
-DB_URI = os.getenv("DATABASE_URL")
-
-# Check if the required environment variables are set
-if not BOT_TOKEN or not DB_URI:
+if not os.environ.get("TOKEN") or not os.environ.get("DATABASE_URL"):
     print("Please specify TOKEN and DATABASE_URL environment variables before starting the bot.")
     exit()
 
-LOG_CHAT = os.getenv("LOG_CHAT")
+from telegram.ext import Filters
 
-# Your bot code can continue from here...
+BOT_TOKEN = os.environ.get("TOKEN")
+DB_URI = os.environ.get("DATABASE_URL")
+SUDO_USERS = [
+    give sudo uers id
+]
+SUDO = Filters.user(SUDO_USERS)
+LOG_CHAT = "Enter your chat id starting from minus, write without quotes"
